cb1fe2b98ab2a638a6206cb657bd328a92742b73
Akash Agrawal <akagr@outlook.com>
2014-08-26 19:01:57 +0530

    Added css minification

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,19 @@
 var gulp = require('gulp'),
 jade = require('gulp-jade');
+minifyCss = require('gulp-minify-css');
 
 /* File assets */
 var source = './src/index.jade';
 var dir = './src/*.jade';
+
+var css = './src/css/*.css'
+
+/* Uglify all src files */
+gulp.task('css', function() {
+  return gulp.src(css)
+  .pipe(minifyCss())
+  .pipe(gulp.dest('./build/css'));
+});
 
 /* Uglify all src files */
 gulp.task('jade', function() {
@@ -15,8 +25,9 @@ gulp.task('jade', function() {
 /* Run related task on file change */
 gulp.task('watch', function() {
   gulp.watch(dir, ['jade']);
+  gulp.watch(css, ['css']);
 });
 
 /* Run this if no task is specified. i.e. Run all tasks and wait for changes */
-gulp.task('default', ['jade', 'watch']);
+gulp.task('default', ['jade', 'css', 'watch']);
 
